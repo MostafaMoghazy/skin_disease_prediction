4fce0d080eaa8046ff7f4d3990ba49970a34f182
Mostafa Moghazy <140920826+MostafaMoghazy@users.noreply.github.com>
2025-08-07 01:16:38 +0300

    Update sapp.py

--- sapp.py
+++ sapp.py
@@ -5,7 +5,7 @@ import tensorflow as tf
 from PIL import Image
 
 # Load model
-model = tf.keras.models.load_model("model.h5")
+model = tf.keras.models.load_model("skindisease.keras")
 
 # Load doctor data
 # doctors_df = pd.read_csv("doctors.csv")
